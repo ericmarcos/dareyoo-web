76d713c725532e39a99e7de3cea4b43e3d6088a6
Eric Marcos <ericmarcos.p@gmail.com>
2014-03-25 10:54:02 +0100

    npm + bower dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,33 @@
+//sudo npm install --save-dev gulp yargs gulp-if gulp-uglify gulp-size gulp-less gulp-rename gulp-replace gulp-git gulp-s3 gulp-clean
+
 var gulp = require('gulp');
+var args   = require('yargs').argv;
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var size = require('gulp-size');
 var less = require('gulp-less');
-var path = require('path');
 var rename = require("gulp-rename");
+var replace = require("gulp-replace");
+var git = require("gulp-git");
+var s3 = require("gulp-s3");
+var clean = require("gulp-clean");
+
+var isPro = args.pro;
+var isPre = args.pre;
 
 var paths = {
-  scripts: ['.static/beta/js/*.js'],
+  html: ['./static/beta/partials/**'],
+  scripts: ['./static/beta/js/*.js'],
   less: './static/beta/less/dareyoo.less',
   less_libs: ['./static/beta/less', './static/beta/lib/bootstrap/less']
 };
+
+gulp.task('html', function(){
+  return gulp.src(paths.html)
+        .pipe(size())
+        //.pipe(replace('PATHX', 'foo'))
+        .pipe(gulp.dest('./static/beta/build/partials'));
+});
 
 gulp.task('scripts', function() {
   // Minify and copy all JavaScript (except vendor scripts)
@@ -15,7 +35,7 @@ gulp.task('scripts', function() {
     .pipe(uglify())
     .pipe(concat('dareyoo.min.js'))
     .pipe(size())
-    .pipe(gulp.dest('./static/beta/js'));
+    .pipe(gulp.dest('./static/beta/build/js'));
 });
 
 gulp.task('less', function () {
@@ -25,13 +45,45 @@ gulp.task('less', function () {
             paths: paths.less_libs
         }))
         .pipe(rename('dareyoo.min.css'))
-        .pipe(gulp.dest('./static/beta/css'));
+        .pipe(gulp.dest('./static/beta/build/css'));
+});
+
+//Replace paths in templates/css/js depending on production/developement
+gulp.task('paths', function () {
+  console.log("Hola!" + args.jeje + args.jaja);
+});
+
+gulp.task('images', function () {
+  console.log("Treating images...");
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.less, ['less']);
+  gulp.watch(paths.less_libs, ['less']);
 });
 
-// The default task (called when you run `gulp` from cli)
-gulp.task('default', ['less', 'watch']);
+// Deploy to S3. TODO (this code is just an example)
+gulp.task('s3', function() {
+  aws = JSON.parse(fs.readFileSync('./aws.json'));
+  options = { delay: 1000 } // optional delay each request by x milliseconds
+  gulp.src('./dist/**', {read: false})
+      .pipe(s3(aws, options));
+});
+
+gulp.task('deploy', function(done){
+  //var options = {args: " -f"};
+  var options = {};
+  git.push('pro', 'master', options, done);
+});
+
+gulp.task('clean', function () {
+    return gulp.src(['build/css', 'build/js', 'build/img', 'build/partials'], {read: false}).pipe(clean());
+});
+
+// Build
+gulp.task('build', ['html', 'less', 'scripts', 'images']);
+
+// Default task (called when you run `gulp` from cli)
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
