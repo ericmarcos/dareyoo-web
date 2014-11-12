aacfa9f8fe326e7d84c22dd32b3c895b3d3341d3
ericmarcos <ericmarcos.p@gmail.com>
2014-11-12 19:13:52 +0100

    global timeline accessible for non logged in users

--- bets/views.py
+++ bets/views.py
@@ -20,6 +20,16 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         return request.method in permissions.SAFE_METHODS or obj.author == request.user
 
 
+class IsAuthenticatedOrIsGlobal(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated() or request.QUERY_PARAMS.get('global')
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        return request.method in permissions.SAFE_METHODS or obj.author == request.user
+
+
 class BetViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     """
     API endpoint that allows bets to be viewed or created, not modified or destroyed.
