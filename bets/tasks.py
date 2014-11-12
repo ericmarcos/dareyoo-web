fb4e5d6661db95415e1cebaadd4d8b628bca482d
ericmarcos <ericmarcos.p@gmail.com>
2014-11-12 18:41:03 +0100

    disabled arbitrating deadline

--- bets/tasks.py
+++ bets/tasks.py
@@ -31,9 +31,9 @@ def complaining_deadline(bet_id=None, **kwargs):
 
 @shared_task(name='arbitrating_deadline')
 def arbitrating_deadline(bet_id=None, **kwargs):
-    b = Bet.objects.get(pk=bet_id)
-    if b.is_arbitrating():
-        b.next_state()
+    #b = Bet.objects.get(pk=bet_id)
+    #if b.is_arbitrating():
+    #    b.next_state()
 
 @shared_task(name='missed_deadlines')
 def missed_deadlines():
