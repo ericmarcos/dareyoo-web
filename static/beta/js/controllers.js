7d1823b173e9e88b51ab09257b271114b276d44f
ericmarcos <ericmarcos.p@gmail.com>
2015-01-27 11:43:09 +0100

    fixed date generation for weekly tournaments

--- static/beta/js/controllers.js
+++ static/beta/js/controllers.js
@@ -252,7 +252,7 @@ angular.module('dareyoo.controllers', []).
         if(response.results) $scope.tournament = response.results;
         else $scope.tournament = response;
         $scope.loaded = true;
-        var max = new Date(Math.min(Date.now(), $scope.tournament.end));
+        var max = new Date(Math.min(Date.now(), new Date($scope.tournament.end)));
         for (var d = new Date($scope.tournament.start); d <= max; d.setDate(d.getDate() + 7)) {
             $scope.weeks.push(new Date(d));
         }
