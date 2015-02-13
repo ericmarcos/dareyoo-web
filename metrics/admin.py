fd59d15b0cfd09d85f268332c5bd82d927d8d8a1
ericmarcos <ericmarcos.p@gmail.com>
2015-02-13 01:01:39 +0100

    enhanced widget metrics admin

--- metrics/admin.py
+++ metrics/admin.py
@@ -4,6 +4,13 @@ from .models import *
 
 class WidgetAdmin(admin.ModelAdmin):
     raw_id_fields = ("bets", "next_bets", )
+    list_display = ('name', 'impressions',
+        'interactions_formatted',
+        'participate_clicks_formatted',
+        'registers_formatted',
+        'logins_formatted',
+        'shares_formatted',
+        'banner_clicks_formatted')
 
 
 admin.site.register(Widget, WidgetAdmin)
--- metrics/models.py
+++ metrics/models.py
@@ -88,6 +88,75 @@ class Widget(models.Model):
             self._random_next_bets = list(self.next_bets.all().bidding().order_by('?'))
         return self._random_next_bets
 
+    def impressions(self):
+        return self.activations.filter(level=1).count()
+
+    def interactions(self):
+        return self.activations.filter(level=2).count()
+
+    def interactions_formatted(self):
+        n = self.interactions()
+        try:
+            return "{0:n} ({0:.0f}%)".format(n, float(n)/self.impressions() * 100)
+        except ZeroDivisionError:
+            return "{0:n} (--)".format(n)
+    interactions_formatted.short_description = 'Interactions'
+
+    def participate_clicks(self):
+        return self.activations.filter(level=3).count()
+
+    def participate_clicks_formatted(self):
+        n = self.participate_clicks()
+        try:
+            return "{0:n} ({0:.0f}%)".format(n, float(n)/self.impressions() * 100)
+        except ZeroDivisionError:
+            return "{0:n} (--)".format(n)
+    participate_clicks_formatted.short_description = 'Participate clicks'
+
+    def registers(self):
+        return self.activations.filter(level=4).count()
+
+    def registers_formatted(self):
+        n = self.registers()
+        try:
+            return "{0:n} ({0:.0f}%)".format(n, float(n)/self.participate_clicks() * 100)
+        except ZeroDivisionError:
+            return "{0:n} (--)".format(n)
+    registers_formatted.short_description = 'Registers'
+
+    def logins(self):
+        return self.activations.filter(level=5).count()
+
+    def logins_formatted(self):
+        n = self.logins()
+        try:
+            return "{0:n} ({0:.0f}%)".format(n, float(n)/self.participate_clicks() * 100)
+        except ZeroDivisionError:
+            return "{0:n} (--)".format(n)
+    logins_formatted.short_description = 'Logins'
+
+    def shares(self):
+        return self.activations.filter(level=6).count()
+
+    def shares_formatted(self):
+        n = self.shares()
+        try:
+            return "{0:n} ({0:.0f}%)".format(n, float(n)/(self.registers() + self.logins()) * 100)
+        except ZeroDivisionError:
+            return "{0:n} (--)".format(n)
+    shares_formatted.short_description = 'Shares'
+
+    def banner_clicks(self):
+        return self.activations.filter(level=7).count()
+
+    def banner_clicks_formatted(self):
+        n = self.banner_clicks()
+        try:
+            return "{0:n} ({0:.0f}%)".format(n, float(n)/self.impressions() * 100)
+        except ZeroDivisionError:
+            return "{0:n} (--)".format(n)
+    banner_clicks_formatted.short_description = 'Banner clicks'
+
     def __unicode__(self):
         return unicode(self.name)
 
