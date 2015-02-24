60409c0b2fd7d9c0cb583be61a258b775ef4692a
ericmarcos <ericmarcos.p@gmail.com>
2015-02-24 16:41:03 +0100

    fixed create widget activation task

--- metrics/tasks.py
+++ metrics/tasks.py
@@ -13,7 +13,7 @@ def save_widget_activation(**kwargs):
         level=kwargs.get('level'),
         from_ip=kwargs.get('from_ip'),
         from_host=kwargs.get('from_host'),
-        participate_result=kwargs.get('result'),
-        medium_shared=kwargs.get('medium'),
-        banner_clicked=kwargs.get('banner')
+        participate_result=kwargs.get('participate_result'),
+        medium_shared=kwargs.get('medium_shared'),
+        banner_clicked=kwargs.get('banner_clicked')
     )
