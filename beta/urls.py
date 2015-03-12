ff4663b71c27122e5c1b08a428d2de5ae17aa0cd
ericmarcos <ericmarcos.p@gmail.com>
2015-03-12 13:08:38 +0100

    removed unnecessary import

--- beta/urls.py
+++ beta/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = patterns('',
