f68de01722a8d8b1fc230da94dea727908546c85
ericmarcos <ericmarcos.p@gmail.com>
2015-03-26 11:22:41 +0100

    added slug and bet_type to notification serializer

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -8,6 +8,18 @@ class NotificationSerializer(serializers.ModelSerializer):
     recipient = DareyooUserShortSerializer(read_only=True)
     #type = serializers.Field(source='get_type_name')
     id = serializers.Field(source='id')
+    bet = serializers.SerializerMethodField('get_bet_slug')
+    bet_type = serializers.SerializerMethodField('get_bet_type')
+
+    def get_bet_slug(self, obj):
+        if obj.bet:
+            return obj.bet.slug
+        return None
+
+    def get_bet_type(self, obj):
+        if obj.bet:
+            return obj.bet.bet_type
+        return None
 
     class Meta:
         model = Notification
